fb0f4e9dbe7f327020a7cbb7c2e79e3ca604d10b
Jānis Bērziņš <janis.berzins@visma.com>
2020-04-02 19:28:17 +0300

    New MDs

--- RCS_2020/RCS/MD3.4/Program.cs
+++ RCS_2020/RCS/MD3.4/Program.cs
@@ -10,6 +10,23 @@ namespace MD3._4
             //1.Izdrukāt visus vārdus.
             //2.Izdrukāt tikai tos vārdus, kuri sākās ar "J".
             //Izmantojam Substring metodi.
+
+            string[] humanNames = { "Juris", "Kristaps", "Jekabs" };
+
+            for (int i = 0; i < humanNames.Length; i++)
+            {
+                Console.WriteLine(humanNames[i]);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < humanNames.Length; i++)
+            {
+                if (humanNames[i].Substring(0,1) == "J")
+                {
+                    Console.WriteLine(humanNames[i]);
+                }
+            }
         }
     }
 }
---
+++ RCS_2020/RCS/MD3.MaxMin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MD3.MaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { -10, 11, 234234, 1212, 3 };
+            
+            int min = int.MinValue;
+            int max = int.MaxValue;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] > min)
+                {
+                    max = intArray[i];
+                }
+            }
+        }
+    }
+}
