5a26aa5eda33966cf1bc8eda0860095419adfd55
Jānis Bērziņš <janis.berzins@visma.com>
2020-04-02 15:35:25 +0300

    new changes

---
+++ RCS_2020/RCS/MD2.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MD2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Izveidot console aplikāciju, kura paprasa lietotājam vai viņš grib pusdienas. 
+            //Ja lietotājs ir ievadījis TRUE, tad uz ekrāna izdrukāt "Jūsu pusdienas ir gatavas", 
+            //ja lietotājs ir ievadījis FALSE, tad "Jums pusdienas nebūs". 
+            //IF ELSE nosacījumā ir jāizmanto bool tipa mainīgais, nevis string tipa mainīgais. 
+            //Lai no lietotāja ievadītā teksta iegūtu bool tipa mainīgo ir jāizmanto bool.Parse, līdzīgi kā ar konvertēšanu uz veselu skaitli.
+
+            Console.WriteLine("Esi izsalcis (TRUE/FALSE)?");
+
+            bool hungry = bool.Parse(Console.ReadLine());
+
+            if (hungry)
+            {
+                Console.WriteLine("Jūsu pusdienas ir gatavas");
+            }
+            else
+            {
+                Console.WriteLine("Jums pusdienas nebūs");
+            }
+
+        }
+    }
+}
---
+++ RCS_2020/RCS/MD2.c/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MD2.c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Izveidot konsoles aplikāciju, kura prasa ievadīt vienu skaitli. Neļaut ievadīt lielāku skaitli par 20,
+            //tādā gadījumā neizpildīt kodu, bet paziņot par neatļautu skaitli lietotājam. Izmantojot while ciklu, izvadīt reizrēķinu līdz šim skaitlim.
+            //Piemērs ievadot skaitli 5(piefiksēt, ka katrā rindā ir 5 skaitļi un ir 5 rindas un visi skaitļi atdalīti ar atstarpēm):
+            //1 2 3 4 5
+            //2 4 6 8 10
+            //3 6 9 12 15
+            //4 8 12 16 20
+            //5 10 15 20 25
+
+            Console.WriteLine("Ievadi skaitli.");
+            int y = int.Parse(Console.ReadLine());
+
+            while (y > 20)
+            {
+
+            }
+
+            int a = 100;
+
+            while (a > 20)
+            {
+                Console.WriteLine("Ievadi skaitli.");
+                a = int.Parse(Console.ReadLine());
+                if (true)
+                {
+
+                }
+            }
+
+
+        }
+    }
+}
