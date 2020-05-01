d352755f6123733f0a15f0d64ab0cc7510ae587d
janisBerz <janis.berzins@hmcts.net>
2020-05-01 14:06:28 +0300

    Adding task3

---
+++ RCS_2020/CSharp/D5_Arrays/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintElemet();
+        }
+
+        public static void PrintElemet() 
+        {
+            Random random = new Random();
+            int[] a = new int[10];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(0, 10);
+            }
+
+            Console.Write("Enter number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i == a.Length; i++)
+            {
+                if (a[i] == b)
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
