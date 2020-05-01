818209e444b5dc27690bce3c884c67430d65344e
janisBerz <janis.berzins@hmcts.net>
2020-05-01 12:52:36 +0300

    New solution

---
+++ RCS_2020/CSharp/D5_Arrays/D5_Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace D5_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sumArray();
+        }
+        //1) Uztaisīt masīvu ar 5 (int) vērtībām.
+        //Cilvēks ievada visas vērtības konsolē.
+        //2) Uztaisīt ciklu, kas sasummē visus masīva elementus
+
+
+        public static void sumArray()
+        {
+            int[] a = new int[5];
+            int result = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write($"Enter value for elemet {i + 1}: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                result += a[i];
+            }
+
+            Console.WriteLine($"Total: {result}");
+        }
+
+    }
+}
